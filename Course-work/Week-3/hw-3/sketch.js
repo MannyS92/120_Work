de0c2409279495dfa98b98c0871fc8a9649114e8
Manny S <estewart810@yahoo.com>
2018-02-07 20:44:32 -0500

    Week 3 Work

---
+++ Course-work/Week-3/hw-3/sketch.js
@@ -0,0 +1,10 @@
+function setup (){
+  //My first function is the setup function and in it , I will create my canvas and its color.
+  createCanvas(
+    //Watched a video on "the coding train" youtube channel and I noticed that she made her canvas the size of the window , so I wanted to give it a try.
+    window.innerWidth,
+    window.innerHeight
+  );
+  //I decided to use the pink because I really enjoyed what it looked like.
+  background('pink');
+  }
