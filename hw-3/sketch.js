604867df382e317eedc326be92a7403a0751b330
Manny S <estewart810@yahoo.com>
2018-02-07 21:50:42 -0500

    Trying to go live

---
+++ hw-3/sketch.js
@@ -0,0 +1,20 @@
+//Declare Setup Function
+function setup (){
+//Watched a video on "the coding train" youtube channel and I noticed that she made her canvas the size of the window , so I wanted to give it a try.
+  createCanvas(
+    window.innerWidth,
+    window.innerHeight
+  );
+  //Colored the background pink
+  background('pink');
+  }
+
+  //Declare Draw function
+
+  function draw(){
+    //define text size using textSize() Function
+    textSize(100);
+    //Using text function have, p5 write Hello World.
+text("Hello World!!", 350, 250)
+
+  }
