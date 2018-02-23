6360c96bbadad18421c8fb507a5242fdd59fcfba
Manny S <estewart810@yahoo.com>
2018-02-23 01:16:30 -0500

    hw-5 update

--- hw-5/sketch.js
+++ hw-5/sketch.js
@@ -1,37 +1,43 @@
-
-
 function setup() {
 //creating canvas using width
     createCanvas( windowWidth, 800 );
-    background(19, 24, 98);
+
 
 }
 
 //defining variable
 let posX = 0;
 let posY = 0;
-let angle = 0;
+let angle = 5;
 
 
 function draw() {
 
   //Controlling the frame rate in relation to where on the x axis the comet is.
-    let target_fr = mouseX * 0.1 + 0.25;
-    frameRate( target_fr/2 );
+    let target_fr = mouseX * 1 + 0.25;
+    frameRate( target_fr);
   //drawing night sky on repeat
     background(37,28,93);
+
+push();
+
+translate(100,100);
   //comet
+
     push();
+        translate(mouseX,mouseY);
         noCursor();
         fill(100);
-        rotate( radians(angle) );
-        ellipse(mouseX,mouseY,60,50);
+        rotate(angle);
+        ellipse(mouseX+10,mouseY,60,50);
       //crater on comet
         fill(75);
-        ellipse(mouseX +5,mouseY +5, 15, 19);
+        ellipse(mouseX + 5,mouseY +5, 15, 19);
       //updating the spin of it
-        angle= angle + 1;
+        angle += (mouseX * 0.0002);
     pop();
+
+pop();
 
 
 
@@ -65,7 +71,7 @@ push();
         ellipse(90,-200 + posY,5,5);
         //star beta
         ellipse(-20,-110 + posY,18,18);
-  
+
         //moon
         fill(255,238,178);
         ellipse(-400,400 + posY,300,300);
